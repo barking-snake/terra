10ee7001b8afe5b586999e93e5d63cd2e6f92f05
Adam Baker <c0baktr@gmail.com>
2017-12-04 23:14:11 -0700

    basic webserver and security group for celes project

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-## terra
+## Project Celes: Terra   
 learning terraform for aws, openshift, and azure   
 ![terra](http://img1.wikia.nocookie.net/__cb20140219002655/finalfantasy/images/b/bd/FFVI_Terra_Branford_Menu_iOS.png)
+
+### Install/Setup   
+
+### Deployment with Locke (Packer)   
+
+### Current State
+Infantile.   
+Terraform spins up a basic webserver (busybox) and applies a defined security group from main.tf. That's it!   
+
--- main.tf
+++ main.tf
@@ -5,8 +5,33 @@ provider "aws" {
 resource "aws_instance" "celes" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.celes.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "w0t" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags {
     Name = "celes-terraform-example"
+    description = "Example Webserver EC2 instance for Project Celes"
+  }
+}
+
+resource "aws_security_group" "celes" {
+  name = "celes-example-web"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "celes-example-web"
+    secure = "nope"
+    description = "Example security group for Project Celes"
   }
 }
